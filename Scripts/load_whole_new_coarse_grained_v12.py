c1c9c5c3dde96cea59425a940a316ca9d1935337
Barak Raveh <barak@salilab.org>
2018-05-22 20:51:00 -0700

    some adjustments to Test_big6

--- Scripts/load_whole_new_coarse_grained_v12.py
+++ Scripts/load_whole_new_coarse_grained_v12.py
@@ -255,7 +255,7 @@ def get_fgs_regions_to_params(is_remove_gle1_and_nup42):
     default_fgp=FGParamsFactory \
                  ( res_from=nan,
                    res_to= nan,
-                   self_k= 1.11,
+                   self_k= 0.95,
                    self_range= 6.00,
                    kap_k= 5.00, # 3.58 originally
                    kap_range= 5.5, # 4.95 originally
@@ -267,11 +267,12 @@ def get_fgs_regions_to_params(is_remove_gle1_and_nup42):
     default_disordered_fgp.kap_k= None
     default_disordered_fgp.kap_range= None
     default_FSFG= default_fgp.get_copy()
-    default_FSFG.self_k= 1.15
+    default_FSFG.self_k= 1.00
     default_FSFG.nonspec_k= 0.01
     default_GLFG= default_fgp.get_copy()
-    default_GLFG.self_k= 1.30
-    default_GLFG.nonspec_k= 0.06
+    default_GLFG.self_k= 1.15
+    default_GLFG.nonspec_k= 0.04
+#    default_GLFG.rest_length_factor= 0.75
     fgs_regions_to_params['Nsp1']= {
         'N': default_GLFG.get_copy(1, 180),
         'C': default_FSFG.get_copy(181, 550),
