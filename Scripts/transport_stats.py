14580e6accdeb8652b0d8ea27206a8a14c2cfc81
Barak Raveh <barak@salilab.org>
2018-06-13 11:00:53 -0700

    make transport stats work in parallel with Pool

--- Scripts/transport_stats.py
+++ Scripts/transport_stats.py
@@ -6,12 +6,59 @@ import re
 import math
 import scipy.stats as stats
 import cPickle as pickle
+import multiprocessing
+import os.path
 
-CACHE_FNAME='TMP.transport_stats_cache.%s.%s.p' % (sys.argv[1], sys.argv[2])
+CACHE_FNAME='TMP.transport_stats_cache.{}.{}.p' .format(os.path.basename(sys.argv[1]),
+                                                       sys.argv[2])
 # confidence interval required
 CONF=0.95
 print sys.argv
 STATS_FROM_SEC=float(sys.argv[2]) # start stats after specified seconds
+
+def pickle_globals(cache_fname):
+    global N
+    global table
+    global n
+    global total_sim_time_sec
+    global processed_fnames
+    global STATS_FROM_SEC
+    print "UPDATING CACHE"
+    with open(cache_fname,'wb') as f:
+        pickle.dump([ N,
+                      table,
+                      n,
+                      total_sim_time_sec,
+                      set(processed_fnames),
+                      STATS_FROM_SEC ]
+                    , f)
+
+def unpickle_or_initialize_globals(cache_fname):
+    global N
+    global table
+    global n
+    global total_sim_time_sec
+    global processed_fnames
+    global cached_stats_from_sec
+    try:
+        with open(cache_fname,'rb') as f:
+            [ N,
+              table,
+              n,
+              total_sim_time_sec,
+              processed_fnames,
+              cached_stats_from_sec
+            ] = pickle.load(f)
+            assert(fnames.issuperset(processed_fnames))
+            assert(cached_stats_from_sec == STATS_FROM_SEC)
+            print("Cache: processed {} files".format(len(processed_fnames)))
+    except:
+        print "NOT USING CACHE"
+        N={}
+        table={}
+        n=0
+        total_sim_time_sec=0.0
+        processed_fnames=set()
 
 def accumulate(dictionary, key, value):
     if key in dictionary:
@@ -40,31 +87,11 @@ def print_stats(N, table, total_sim_time_sec):
         print key, '%8.1f +- %8.2f /sec/particle (confidence %d%%);   time for all particles=%.3f [sec]' % \
             (mean, get_sems_for_conf(CONF)*sem, CONF*100, total_sim_time_sec*N[key])
 
-############# Main ############
-fnames=set(glob.glob(sys.argv[1]+"*.pb"))
-try:
-    with open(CACHE_FNAME,'rb') as f:
-        [ N,
-          table,
-          n,
-          total_sim_time_sec,
-          processed_fnames,
-          cached_stats_from_sec
-          ] = pickle.load(f)
-    assert(fnames.issuperset(processed_fnames))
-    assert(cached_stats_from_sec == STATS_FROM_SEC)
-    print "Cache: processed", len(processed_fnames), "files"
-except:
-    print "NOT USING CACHE"
-    N={}
-    table={}
-    n=0
-    total_sim_time_sec=0.0
-    processed_fnames=set()
-#modulus = int(sys.argv[2])
-fnames=fnames.difference(processed_fnames)
-print "Processing", len(fnames), " files that are not present in cache"
-for fname in fnames:
+
+def open_file(fname, processed_fnames):
+    global STATS_FROM_SEC
+
+    print("Opening {}".format(fname))
     is_loaded=False
     try:
 #        i = get_output_num(fname)
@@ -72,48 +99,114 @@ for fname in fnames:
         #        continue
         output= Output()
         with open(fname, "rb") as f:
-            output.ParseFromString(f.read())
+            output= Output()
+            fstring= f.read()
+            output.ParseFromString(fstring)
+            print("Parsed {}".format(fname))
         start_sim_time_sec= output.statistics.global_order_params[0].time_ns*(1e-9)
         end_sim_time_sec= output.statistics.global_order_params[-1].time_ns*(1e-9)
         sim_time_sec = end_sim_time_sec-max(start_sim_time_sec,STATS_FROM_SEC)
-        is_loaded=True
     except ValueError as e:
-        print "EXCEPTION ", fname, e
+        print("EXCEPTION {} {}".format(fname, e))
+        raise
     except:
-        print "EXCEPTION ", fname
-    if not is_loaded:
-        continue
-    processed_fnames.add(fname)
+        print("EXCEPTION {}".format(fname))
+        raise
     if sim_time_sec<0:
-        print "SKIP", fname, " ", sim_time_sec
-        continue
-    total_sim_time_sec= total_sim_time_sec+sim_time_sec
-    for floater in output.assignment.floaters:
-        cur_N=floater.number.value
-        if floater.type in N:
-            assert(N[floater.type]==cur_N)
-        else:
-            N[floater.type]=cur_N
-    for floater in output.statistics.floaters:
-#        accumulate(table,floater.type,floater.avg_n_transports);
+        print("SKIP negative normalized sim-time {} stats from sec {}" \
+              .fomrat(sim_time_sec, STATS_FROM_SEC))
+        return None
+    Ns_dict={}
+    counts_dict={}
+    for floater_a in output.assignment.floaters:
+        Ns_dict[floater_a.type]= floater_a.number.value
+    for floater_s in output.statistics.floaters:
         count=0.0
-        for t_ns in floater.transport_time_points_ns:
+        for t_ns in floater_s.transport_time_points_ns:
             t_sec=t_ns*1e-9
             if t_sec>STATS_FROM_SEC:
                 count=count+1.0
-        accumulate(table, floater.type, count/N[floater.type])
-    n=n+1
-    #    total_sim_time_sec=total_sim_time_sec+output.assignment.simulation_time_ns*(1e-9)
-    if(n%10>0 or n==0):
-        continue
-    with open(CACHE_FNAME,'wb') as f:
-        pickle.dump([ N,
-                      table,
-                      n,
-                      total_sim_time_sec,
-                      processed_fnames,
-                      STATS_FROM_SEC
-                      ]
-                    , f)
+        counts_dict[floater_s.type]= count
+    file_summary={"fname":fname,
+                  "sim_time_sec":sim_time_sec,
+                  "Ns_dict": Ns_dict,
+                  "counts_dict":counts_dict}
+    print("Opened {}".format(fname))
+    processed_fnames.append(fname)
+    return file_summary
 
+
+
+
+def _sum_output_stats_exception(file_summary):
+    global N
+    global table
+    global n
+    global processed_fnames
+    global CACHE_FNAME
+    global STATS_FROM_SEC
+    global total_sim_time_sec
+    cache_frequency=500
+
+    if file_summary is None:
+        print("Empty output skipped")
+    total_sim_time_sec= total_sim_time_sec+file_summary["sim_time_sec"]
+    for floater_type, floater_N in file_summary["Ns_dict"].iteritems():
+        if floater_type in N:
+            assert(N[floater_type]==floater_N)
+        else:
+            N[floater_type]=floater_N
+    for floater_type, count in file_summary["counts_dict"].iteritems():
+        accumulate(table, floater_type, count/N[floater_type])
+    n=n+1
+    if(len(processed_fnames) % 10 == 0):
+        print("Number of files processed {0:d}".format(len(processed_fnames)))
+    if(len(processed_fnames) % cache_frequency == 0):
+        if CACHE_FNAME is not None:
+            pickle_globals(CACHE_FNAME)
+
+def sum_output_stats(file_summary):
+    try:
+        _sum_output_stats_exception(file_summary)
+    except:
+        if file_summary is not None:
+            print("Exception summarizing stats of {0}".format(file_summary["fname"]))
+        else:
+            print("Unknown exception in sum_output_stats()")
+        raise
+
+
+
+############# Main ############
+fnames=set(glob.glob(sys.argv[1]+"*.pb"))
+unpickle_or_initialize_globals(CACHE_FNAME)
+#modulus = int(sys.argv[2])
+fnames=fnames.difference(processed_fnames)
+print "Processing", len(fnames), " files that are not present in cache"
+pool= multiprocessing.Pool(processes=8)
+manager= multiprocessing.Manager()
+processed_fnames= manager.list(processed_fnames)
+print("Starting pool")
+R=[]
+fnames=list(fnames)
+for fname in fnames:
+    try:
+#        sum_output_stats(open_file(fname, processed_fnames))
+        r= pool.apply_async(open_file,
+                            args=(fname, processed_fnames),
+                            callback=sum_output_stats)
+        R.append(r)
+    except:
+        print("Failed on {0}".format(fname))
+pool.close()
+pool.join()
+print("Pool closed and joined")
+print("Verifying status of individual processes...")
+for i,r in enumerate(R):
+    try:
+        r.get()
+    except:
+        print("Process {:d} failed".format(i))
+print("Pickling and reporting stats:")
+pickle_globals(CACHE_FNAME)
 print_stats(N, table, total_sim_time_sec)
